c8398a0afb17e32032c06bfbb8c80978d9f3e4bc
IndongLee <kundera@kakao.com>
2020-12-23 23:20:31 +0900

    map, flatmap, buffer, scan, groupby

--- 02_RxJava/src/main/kotlin/main/Operator.kt
+++ 02_RxJava/src/main/kotlin/main/Operator.kt
@@ -11,6 +11,13 @@ fun main() {
 
     interval()
     range()
+    timer()
+
+    map()
+    flatMap()
+    buffer()
+    scan()
+    groupBy()
 }
 
 fun defer() {
@@ -68,4 +75,66 @@ fun timer() {
     println("timer 구독")
     src.subscribe { println("timer 실행!") }
     Thread.sleep(1001L)
+}
+
+fun map() {
+    Observable.just(1, 2, 3)
+        .map {
+            it * 10
+        }.subscribe {
+            println(it)
+        }
+}
+
+fun flatMap() {
+    Observable.just("a", "b", "c")
+        .flatMap {
+            Observable.just("${it}1", "${it}2")
+        }.subscribe {
+            println(it)
+        }
+}
+
+fun buffer() {
+    Observable.range(0, 10)
+        .buffer(3)
+        .subscribe { numbers ->
+            println("버퍼 데이터 발행")
+            numbers.forEach {
+                println("#$it")
+            }
+        }
+}
+
+fun scan() {
+    Observable.range(1, 5)
+        .scan { t1: Int, t2: Int ->
+            println("$t1 + $t2 = ${t1 + t2}")
+            t1 + t2
+        }.subscribe()
+}
+
+fun groupBy() {
+    fun classify(item: String): String {
+        return when(item.split(" ")[1]) {
+            "Circle" -> "C"
+            "Triangle" -> "T"
+            else -> "None"
+        }
+    }
+    Observable.just(
+        "Magenta Circle",
+        "Cyan Circle",
+        "Yellow Triangle",
+        "Yellow Circle",
+        "Magenta Triangle",
+        "Cyan Triangle"
+    ).groupBy { item ->
+        classify(item)
+    }.subscribe { group ->
+        println("${group.key} 그룹 발행 시작")
+        group.subscribe {
+            println("${group.key} : $it")
+        }
+    }
 }
